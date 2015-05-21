1b1e35e09c5b4a2c78426a87811701c8afcf2c8b
Gergely Nagy <algernon@madhouse-project.org>
2015-05-21 13:08:44 +0200

    examples: Update the madhouse-project.org example
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- examples/madhouse-project.org.js
+++ examples/madhouse-project.org.js
@@ -10,22 +10,29 @@ module.exports = {
         source: {host: "10.243.42.34"}
     },
     widgets: [
-        {widget: ph.widgets.log,
-         options: {label: "Events @ morgoth",
-                   source: {query: 'host = "morgoth"'}
-                  },
-         width: 6,
-         height: 6},
-        {widget: ph.widgets.log,
-         options: {label: "Events @ eowyn",
-                   source: {query: 'host = "eowyn"'}
-                  },
-         width: 6,
-         height: 6},
         {widget: ph.widgets.line_chart,
          options: {label: "Memory usage %",
                    source: {query: 'service = "memory/percent-used"'}},
          width: 12,
-         height: 6}
+         height: 9},
+
+        {widget: ph.widgets.log,
+         options: {label: "Events @ morgoth",
+                   source: {query: 'host = "morgoth"'}
+                  },
+         width: 4,
+         height: 3},
+        {widget: ph.widgets.log,
+         options: {label: "Events @ beleg",
+                   source: {query: 'host = "beleg"'}
+                  },
+         width: 4,
+         height: 3},
+        {widget: ph.widgets.log,
+         options: {label: "Events @ eowyn",
+                   source: {query: 'host = "eowyn"'}
+                  },
+         width: 4,
+         height: 3},
     ]
 };
